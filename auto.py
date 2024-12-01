27d76481c2eb79bca921c30959fba9d5ca9ddbe8
anhduy0617 <duy596912@gmail.com>
2024-12-01 21:09:26 +0700

    day la thay doi cua duy

--- auto.py
+++ auto.py
@@ -1,6 +1,6 @@
-import time.
+import time
 from netmiko import ConnectHandler, NetMikoTimeoutException, NetMikoAuthenticationException
-
+print("toi la duy")
 # Định nghĩa các router.
 R1 = {
     "device_type": "mikrotik_routeros",
